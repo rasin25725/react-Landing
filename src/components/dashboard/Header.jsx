7d38c341d6be4d748d98dc62c24ac193060ddd35
rasin25725 <rasin25725@gmail.com>
2025-02-18 15:06:29 +0530

    second commit

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
-function Header(){
-    return <div>
-    Header
+import React from "react";
+import icon from "../../assets/react.svg";
+function Header() {
+  return (
+    <div className="bg-[#000] flex justify-between items-center">
+      <div className="flex items-center gap-4 p-4">
+        <h1 className="w-[40px] h-[40px] mr-2">
+          <img src={icon} alt="" width={100} height={100} />
+        </h1>
+        <h4 className="text-[#fff] text-[24px]">tailwindss</h4>
+      </div>
+      <div>
+        <ul className="text-[#fff] flex gap-6">
+          <li>Docs</li>
+          <li>Components</li>
+          <li>Blog</li>
+          <li>GitHub</li>
+        </ul>
+      </div>
     </div>
+  );
 }
-export default Header
+
+export default Header;
